2f8fbc880decd2a5b1eacf8beea0f68b674a9631
Chelsea Wagner <1chelsea.fischer1@gmail.com>
2023-11-11 21:06:21 -0800

    updated signupForm and LoginForm with mutation functionality

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,11 +1,13 @@
 // see SignupForm.js for comments
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+import { LOGIN_USER } from '../utils/mutations';
 
-import { loginUser } from '../utils/API';
+//import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
+  const [loginUser, {error}] = useMutation(LOGIN_USER);
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
@@ -26,7 +28,7 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      const response = await loginUser(LOGIN_USER ,{variables: { ...userFormData }});
 
       if (!response.ok) {
         throw new Error('something went wrong!');
--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { createUser } from '../utils/API';
+// import { createUser } from '../utils/API';
+import{ADD_USER} from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
+  const [createUser, {error}] = useMutation(ADD_USER);
+
   // set initial form state
+
   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
   // set state for form validation
   const [validated] = useState(false);
@@ -28,7 +32,7 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const response = await createUser(ADD_USER, {variables: { ...userFormData }});
 
       if (!response.ok) {
         throw new Error('something went wrong!');

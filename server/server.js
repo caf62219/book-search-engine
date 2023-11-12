d338ffedd89e1e5afddef8aa6fbec3c7d6ea60e5
Chelsea Wagner <1chelsea.fischer1@gmail.com>
2023-11-11 19:43:06 -0800

    added schemas file and added index.js, updated auth.js to include functioning middleware for GraphQL API and updated serverJS to implement apollo server and applied express server as middleware

--- server/server.js
+++ server/server.js
@@ -1,21 +1,40 @@
 const express = require('express');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+const { typeDefs, resolvers } = require('./schemas');
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const startApolloServer = async () => {
+  await server.start();
+
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  }
+};
 
-app.use(routes);
+app.use('/graphql', expressMiddleware(server));
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
 });
+})  
+
+startApolloServer();
